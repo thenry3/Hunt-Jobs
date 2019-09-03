0aee467df7a76b6f380713b38a0790d420f4b1ee
thenry3 <43651306+thenry3@users.noreply.github.com>
2019-09-03 15:23:20 -0700

    Uncommented indeed code

--- __main__.py
+++ __main__.py
@@ -25,14 +25,14 @@ if __name__ == "__main__":
     JobType = "internship" if args.internship else "fulltime" if args.fulltime else ""
     keywords = args.keywords if args.keywords is not None else [""]
     glassdoorJobs = scrapes.glassdoor(args.locations, args.keywords, JobType)
-    # indeedJobs = scrapes.indeed(args.locations, args.keywords, JobType)
+    indeedJobs = scrapes.indeed(args.locations, args.keywords, JobType)
 
     index = 0
-    # for company in indeedJobs:
-    #     for post in indeedJobs[company]:
-    #         df.loc[index] = [company, post["job"], post["link"],
-    #                          post["location"], post["posted"], post["meta"]]
-    #         index += 1
+    for company in indeedJobs:
+        for post in indeedJobs[company]:
+            df.loc[index] = [company, post["job"], post["link"],
+                             post["location"], post["posted"], post["meta"]]
+            index += 1
 
     for company in glassdoorJobs:
         for post in glassdoorJobs[company]:
